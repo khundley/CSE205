5096088fe4a433f702864ce520eec8a48f45ea23
Team5-TonyS <108765973+Team5-TonyS@users.noreply.github.com>
2022-07-05 18:00:20 -0400

    Update H01_37.java

--- khundley-h01/H01_37.java
+++ khundley-h01/H01_37.java
@@ -1,15 +1,23 @@
-// CLASS: H01_037
-// AUTHOR: ANTHONY SPAUGH, ASPAUGH, ASPAUGH@ASU.EDU
+/* 
+CLASS: H01_37
+
+TEAM: TEAM 5
+AUTHORS:
+    ANDREW BRAND   | ABRAND3  | A.BRAND101691@YAHOO.COM
+    KENYON HUNDLEY | KHUNDLEY | KENYONHUNDLEY@YAHOO.COM
+    ISAIAH POTTS   | IPOTTS1  | ZAHPOTTS123@GMAIL.COM
+    ANTHONY SPAUGH | ASPAUGH  | ASPAUGH@ASU.EDU
+*/
 
 import java.util.ArrayList;
 
 public class H01_37 {
 	
-	//CONSTRUCTOR
+    //CONSTRUCTOR
     public H01_37() {
     }
 	
-	//SOLUTION METHOD
+    //SOLUTION METHOD
     public void arrayListRemove(ArrayList<Integer> pList, int pValue) {
         for (int index = 0; index < pList.size(); index++) {
             if (pList.get(index) == pValue) {
