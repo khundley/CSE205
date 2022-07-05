04d69adc9b2c189bde1f9e6682bc68c029ed1e8b
Team5-TonyS <108765973+Team5-TonyS@users.noreply.github.com>
2022-07-05 17:28:42 -0400

    Add files via upload

---
+++ khundley-h01/H01_37.java
@@ -0,0 +1,21 @@
+// CLASS: H01_037
+// AUTHOR: ANTHONY SPAUGH, ASPAUGH, ASPAUGH@ASU.EDU
+
+import java.util.ArrayList;
+
+public class H01_37 {
+	
+	//CONSTRUCTOR
+    public H01_37() {
+    }
+	
+	//SOLUTION METHOD
+    public void arrayListRemove(ArrayList<Integer> pList, int pValue) {
+        for (int index = 0; index < pList.size(); index++) {
+            if (pList.get(index) == pValue) {
+                pList.remove(index);
+                index--;
+            }
+        }
+    }
+}
