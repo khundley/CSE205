e372fc5a117bfd0eaaa8c96f880013c56a33d2ee
Team5-TonyS <108765973+Team5-TonyS@users.noreply.github.com>
2022-07-11 19:31:12 -0400

    Create Main.java

---
+++ Team_5-p02/Main.java
@@ -0,0 +1,17 @@
+//********************************************************************************************************
+// CLASS: Main
+//
+// DESCRIPTION
+// 
+// 
+// 
+//
+// COURSE AND PROJECT INFO
+// CSE205 Object Oriented Programming and Data Structures, Summer Session B 2022 // Project Number: 02
+// TEAM: TEAM 5
+// AUTHORS:
+// ANDREW BRAND   | ABRAND3  | A.BRAND101691@YAHOO.COM
+// KENYON HUNDLEY | KHUNDLEY | KENYONHUNDLEY@YAHOO.COM
+// ISAIAH POTTS   | IPOTTS1  | ZAHPOTTS123@GMAIL.COM
+// ANTHONY SPAUGH | ASPAUGH  | ASPAUGH@ASU.EDU
+//********************************************************************************************************
