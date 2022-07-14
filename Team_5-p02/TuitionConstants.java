a707b7d1121c2c77767838cea637ef136a6063bd
Team5-TonyS <108765973+Team5-TonyS@users.noreply.github.com>
2022-07-13 20:51:20 -0400

    Create TuitionConstants.java

---
+++ Team_5-p02/TuitionConstants.java
@@ -0,0 +1,24 @@
+//********************************************************************************************************
+// CLASS: TuitionConstants
+//
+// DESCRIPTION
+// This class maintains the constant values used to calcuate tuition.
+//
+// COURSE AND PROJECT INFO
+// CSE205 Object Oriented Programming and Data Structures, Summer Session B 2022 // Project Number: 02
+// TEAM: TEAM 5
+// AUTHORS:
+// ANDREW BRAND   | ABRAND3  | A.BRAND101691@YAHOO.COM
+// KENYON HUNDLEY | KHUNDLEY | KENYONHUNDLEY@YAHOO.COM
+// ISAIAH POTTS   | IPOTTS1  | ZAHPOTTS123@GMAIL.COM
+// ANTHONY SPAUGH | ASPAUGH  | ASPAUGH@ASU.EDU
+//********************************************************************************************************
+public class TuitionConstants {
+
+    public static final int ONCAMP_ADD_CREDITS  = 475;
+    public static final int ONCAMP_MAX_CREDITS  = 18;
+    public static final int ONCAMP_NONRES_BASE  = 14875;
+    public static final int ONCAMP_RES_BASE     = 7575;
+    public static final int ONLINE_CREDIT_RATE  = 950;
+    public static final int ONLINE_TECH_FEE     = 75;
+}
